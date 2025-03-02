a9565af8bbb07c4e1a577f3a49833e154f187e49
Foreversengine <opeloyeruwasiu123@gmail.com>
2025-03-02 11:50:16 +0000

    0. Implementing Advanced Model Relationships in Django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from relationship_app.models import Author, Book, Library, Librarian
 
-# Create your views here.
+def author_books(request, author_name):
+    author = Author.objects.get(name=author_name)
+    books = author.books.all()
+    return render(request, 'author_books.html', {'author': author, 'books': books})
