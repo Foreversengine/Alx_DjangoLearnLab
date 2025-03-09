851124ee122d2d2902fdb00b424ec5b72e275a05
Foreversengine <opeloyeruwasiu123@gmail.com>
2025-03-09 12:10:25 +0000

    Advanced Features and Security

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,16 +1,35 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 from .models import Book
 
-# Custom Admin Class for the Book Model
+# Register your models here.
+@admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    # Display these fields in the list view
-    list_display = ('title', 'author', 'publication_year')
+    list_display = ["id", "title", "author", "publication_year"]
+    list_filter = ["publication_year", "author"]
+    search_fields = ["title", "author"]
+    
 
-    # Add filters for these fields
-    list_filter = ('author', 'publication_year')
 
-    # Enable search for these fields
-    search_fields = ('title', 'author')
 
-# Register the Book model with the custom admin class
-admin.site.register(Book, BookAdmin)
+# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ("email", "username", "date_of_birth", "is_staff", "is_active")
+    list_filter = ("is_staff", "is_active")
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal Info", {"fields": ("username", "date_of_birth", "profile_photo")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "is_superuser")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "username", "date_of_birth", "profile_photo", "password1", "password2", "is_staff", "is_active"),
+        }),
+    )
+    search_fields = ("email", "username")
+    ordering = ("email",)
+
+admin.site.register(CustomUser, CustomUserAdmin)
